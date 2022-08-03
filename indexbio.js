246fdb9263b9e8a86ec6cda9dd97cff727cdb8c4
Infinity <60041234+TrainsRAwesome@users.noreply.github.com>
2022-08-03 08:49:01 +0100

    new index bio

--- indexbio.js
+++ indexbio.js
@@ -1,6 +1,7 @@
 var i = 0;
-var txt = 'A developer with a love for creating the things that once challenged me';
-var speed = 50;
+const txts = ['A developer with a love for creating the things that once challenged me','A person who codes random stuff whenever they feel like it','Another dude who found python annoying and, well, doesn\'t use it anymore'];
+const txt = txts[Math.floor(Math.random() * txts.length)];
+const speed = 50;
 
 function typeWriter() {
   if (i < txt.length) {

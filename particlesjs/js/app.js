e176aac240083fdbc2e01c109bc8768625a32945
TrainsRAwesome <jamesmsouthall@yahoo.co.uk>
2022-10-20 19:03:18 +0100

    reduced snow

--- particlesjs/js/app.js
+++ particlesjs/js/app.js
@@ -17,7 +17,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 400,
+        "value": 100,
         "density": {
           "enable": true,
           "value_area": 800

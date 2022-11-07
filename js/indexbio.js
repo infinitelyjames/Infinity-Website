a0b4ada4a9d04d7528207d5123eaf5ad805651df
TrainsRAwesome <jamesmsouthall@yahoo.co.uk>
2022-11-07 17:48:35 +0000

    fixed bad grammar

--- js/indexbio.js
+++ js/indexbio.js
@@ -1,5 +1,5 @@
 var i = 0;
-const txts = ['A developer with a love for creating the things that once challenged me','A person who codes random stuff whenever they feel like it','Another dude who found python annoying and, well, doesn\'t use it anymore'];
+const txts = ['A developer with a love for creating the things that once challenged them','A person who codes random stuff whenever they feel like it','Another dude who found python annoying and, well, doesn\'t use it anymore'];
 const txt = txts[Math.floor(Math.random() * txts.length)];
 const speed = 50;
 
